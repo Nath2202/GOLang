819164121ef84535278f61aa3670ea9a118d1ed6
Nathalia Lourenço <33758703+Nath2202@users.noreply.github.com>
2018-02-26 15:31:19 -0300

    Novo Capitulo

---
+++ Pacotes/Reta/reta.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"math"
+)
+
+type Ponto struct {
+	x float64
+	y float64
+}
+
+//funções, variáveis ou estruturas que tem o seu nome iniciado com letra minuscula, tem sua visibilidade PRIVADA
+//funções, variáveis ou estruturas que tem o seu nome iniciado com letra maiuscula, tem sua visibilidade PÚBLICA
+
+func catetos(a, b Ponto) (cx, cy float64) {
+	cx = math.Abs(b.x - a.x) //ponto representa uma coordenada no plano cartesiano
+	cy = math.Abs(b.y - a.y)
+	return
+
+}
+
+func Distancia(a, b Ponto) float64 {
+	cx, cy := catetos(a, b)
+	return math.Sqrt(math.Pow(cx, 2) + math.Pow(cy, 2))
+}
