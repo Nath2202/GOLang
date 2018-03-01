9608a2c9360e6ee5cbe7ba3841c4b44218fa7664
Nathalia Lourenço <33758703+Nath2202@users.noreply.github.com>
2018-03-01 11:25:00 -0300

    New Folders

---
+++ Pacotes/Reuso/reuso.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Nath2202/area"
+)
+
+func main() {
+	fmt.Println(area.Circ(2.0))
+	fmt.Println(area.Rect(2.0, 4.0))
+
+}
---
+++ Pacotes/UsoLib/usolib.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Nath2202/goarea"
+)
+
+func main() {
+	fmt.Println(goarea.Circ(4.0))
+	
+
+}
